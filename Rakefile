97bf86bfcf56377ed664d4112ea191616551d18d
Dmitry Plashchynski <plashchynski@gmail.com>
2011-10-15 15:22:50 +0300

    add few benchmarks for array

--- Rakefile
+++ Rakefile
@@ -2,6 +2,8 @@ task :default => :bench
 
 desc "Run all the benchmarks"
 task :bench do
+  puts RUBY_DESCRIPTION
+  printf "%-35s user     system      total        real\n", ''
   require File.dirname(__FILE__) + '/lib/benchmark'
   require File.dirname(__FILE__) + '/benchmarks/array.rb'
   require File.dirname(__FILE__) + '/benchmarks/string.rb'
--- benchmarks/array.rb
+++ benchmarks/array.rb
@@ -1,18 +1,34 @@
 @input_data = [0,1000,10_000]
 @count = 100_000
 
-benchmark("[].first", @input_data, @count) do |array|
+benchmark("array.first", @input_data, @count) do |array|
   array.first
 end
 
-benchmark("[][0]", @input_data, @count) do |array|
+benchmark("array[0]", @input_data, @count) do |array|
   array[0]
 end
 
-benchmark("[].last", @input_data, @count) do |array|
-  array.first
+benchmark("array.last", @input_data, @count) do |array|
+  array.last
 end
 
-benchmark("[][-1]", @input_data, @count) do |array|
+benchmark("array[-1]", @input_data, @count) do |array|
   array[-1]
+end
+
+benchmark("array.map(&:to_i)", [0,100,1000], @count) do |array|
+  array.map(&:to_i)
+end
+
+benchmark("array.map{|n| n.to_i }", [0,100,1000], @count) do |array|
+  array.map{|n| n.to_i }
+end
+
+benchmark("[]", nil, @count) do |array|
+  []
+end
+
+benchmark("Array.new", nil, @count) do |array|
+  Array.new
 end
