faeffaa5d1681c21f3d263f8247fe7e44591aa26
Dmitry Plashchynski <plashchynski@gmail.com>
2011-10-27 19:07:39 +0300

    add benchmark for array.each and for

--- benchmarks/array.rb
+++ benchmarks/array.rb
@@ -17,6 +17,16 @@ benchmark("array[-1]", @input_data, @count) do |array|
   array[-1]
 end
 
+benchmark("array.each do |item|", [10000], 10000) do |array|
+  array.each do |item|
+  end
+end
+
+benchmark("for item in array do", [10000], 10000) do |array|
+  for item in array do
+  end
+end
+
 benchmark("array.map(&:to_i)", [0,100,1000], @count) do |array|
   array.map(&:to_i)
 end
